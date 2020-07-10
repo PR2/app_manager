28598155036c6a121ebf59e8c1629d956a2bb585
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-07-10 22:59:07 +0900

    import _thread as thread

--- src/app_manager/app_manager.py
+++ src/app_manager/app_manager.py
@@ -36,7 +36,7 @@
 import sys
 
 if sys.version_info[0] == 3:
-    import _thread # python3 renamed from thread to _thread
+    import _thread as thread  # python3 renamed from thread to _thread
 else:
     import thread
 
